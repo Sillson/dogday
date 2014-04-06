de257bf5633c5f4976073eea963a744dbcd0475b
Stuart Illson <sillson88@gmail.com>
2014-04-05 19:27:25 -0700

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogDay::Application.config.secret_key_base = '3525905d46c2c96d0ea5a9016d467a4626b0ef2db61fe3a31bf6abe8038a5add326dfa9a639248d262f3ddc6d3a7e21f0c17047c953cb6dc1b8dea72caa6ea36'
